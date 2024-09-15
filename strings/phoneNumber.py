c0f57d317af8067a947537982d44a887dc17e07d
Michael Malver <mmalver@gmail.com>
2024-09-14 19:40:10 -0500

    testing the join function of str

---
+++ strings/phoneNumber.py
@@ -0,0 +1,6 @@
+areaCode = input("Enter areaCOde: ")
+firstThree = input("Enter the first three digits of a phone number: ")
+lastFour = input("Enter the lat four digits of a phone number: ")
+number = "-".join([areaCode,firstThree,lastFour])
+print(number)
+
