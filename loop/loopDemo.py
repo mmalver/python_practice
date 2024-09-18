06c1487eafbdc8e67068e11816b792754c646311
Michael Malver <mmalver@gmail.com>
2024-09-18 16:37:07 -0500

    demo of for each loops

---
+++ loop/loopDemo.py
@@ -0,0 +1,9 @@
+dogs = ["pria","fletch","suave","haret","belvedere","hubert"]
+
+print("This is a list of dog names.")
+for dog in dogs:
+    print(dog)
+
+print("This is a list of numbers")
+for number in range(2,21,2):
+    print(number)
