26af77b1d3285dafa518db66ecb55ad8227fb863
Michael Malver <mmalver@gmail.com>
2024-09-25 16:41:22 -0500

    sample dictionary

---
+++ dictionaries/fiftyfifty.py
@@ -0,0 +1,7 @@
+# dictionary of tiket number and names
+
+fiftyfifty = {1 : "Michael", 5 : "Janet", 9 : "Marion"}
+print(f"The winner is {fiftyfifty[9]}")
+
+for item in fiftyfifty:
+    print(f"The key is {item} and the value is {fiftyfifty[item]}")
