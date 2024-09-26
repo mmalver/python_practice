a6b8b419c5c979580881b59609089ec7cac06c01
Michael Malver <mmalver@gmail.com>
2024-09-26 17:07:58 -0500

    getting key value pairs in one line when looping

---
+++ dictionaries/fiftyfiftyproperloop.py
@@ -0,0 +1,7 @@
+# dictionary of tiket number and names
+
+fiftyfifty = {1 : "Michael", 5 : "Janet", 9 : "Marion"}
+print(f"The winner is {fiftyfifty[9]}")
+
+for number, name  in fiftyfifty.items():
+    print(f"The key is {number} and the value is {name}")
