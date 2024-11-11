690f2ca576ef900bc39ff4d97c05b35370d8317c
Michael Malver <mmalver@gmail.com>
2024-11-11 08:22:47 -0600

    demoing the if statement for evaluating if string is empty or not

---
+++ strings/emptyStringEval.py
@@ -0,0 +1,12 @@
+myString = "This string has data"
+myString2 = ""
+
+if myString:
+    print(f"The value of the string is {myString}")
+else:
+    print("This string is empty.")
+
+if myString2:
+    print(f"The value of my string 2  is {myString2}")
+else:
+    print("myString2 is empty.")
---
+++ strings/stringAndIf.py
@@ -0,0 +1,11 @@
+name1 = ""
+if name1:
+    print("I contain data.")
+else:
+    print("I am empty.")
+
+name2 = "name"
+if name2:
+    print("I contain data.")
+else:
+    print("I am empty.")
